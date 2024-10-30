1160bb7b0f624244a26eec5bbbf98174c40f887f
Qwest1204 <myPCwithUbuntu@gmail.com>
2024-10-30 06:48:05 +0300

    add link to github

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,8 @@ class Header extends React.Component {
         <div class="container">
             <header class="d-flex justify-content-center py-3">
               <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link">Проекты</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 link-body-emphasis">Проекты и статьи</a></li>
+                <li><a href="https://github.com/Qwest1204" class="nav-link px-2 link-body-emphasis">Github</a></li>
               </ul>
             </header>
           </div>
